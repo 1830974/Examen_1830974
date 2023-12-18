b1a2d127bab37b9f82e234beb40baf0010a3ca61
Brandon Tremblay <1830974@etu.cchic.ca>
2023-12-18 14:26:53 -0500

    Version Finale : le correction de produits vérifie maintenant si le code de produtis fourni correspond à un produit avant d'en faire la correction (oublie dans les étapes antérieure), La fonction de mise à jour calcul les prix et affiche la liste de produits à sérialisesr, ce qui permet de s'assurer que nous allons sauvegarder les bonnes données dans le fichier JSON

--- Examen_Final/StockLibrary/StockService.cs
+++ Examen_Final/StockLibrary/StockService.cs
@@ -30,22 +30,25 @@ namespace StockLibrary
                 {
                     foreach (var produit in produits)
                     {
-                        //Total des taxes applicable sur un produit
-                        double taxes = 1 + produit.TPS + produit.TVQ;
-
-                        //Calcul du prix avant taxes dans le cas où les taxes ne sont pas de 0 (Éviter la division par 0)
-                        if (taxes != 0)
+                        if (produit.CodeProduit == CodeProduit)
                         {
-                            produit.PrixAvantTaxes = produit.PrixApresTaxes / taxes;
-                        }
-                        else 
-                        {
-                            //Si les taxes étaient de 0, le prix après taxes est le même que celui avant taxes
-                            produit.PrixAvantTaxes = produit.PrixApresTaxes;
-                        }
+                            //Total des taxes applicable sur un produit
+                            double taxes = 1 + produit.TPS + produit.TVQ;
 
-                        //Affichage du produit et de son prix avant taxes
-                        Console.WriteLine("{0} {1} {2}", produit.CodeProduit, produit.NomProduit, produit.PrixAvantTaxes);
+                            //Calcul du prix avant taxes dans le cas où les taxes ne sont pas de 0 (Éviter la division par 0)
+                            if (taxes != 0)
+                            {
+                                produit.PrixAvantTaxes = produit.PrixApresTaxes / taxes;
+                            }
+                            else 
+                            {
+                                //Si les taxes étaient de 0, le prix après taxes est le même que celui avant taxes
+                                produit.PrixAvantTaxes = produit.PrixApresTaxes;
+                            }
+
+                            //Affichage du produit et de son prix avant taxes
+                            Console.WriteLine("{0} {1} {2}", produit.CodeProduit, produit.NomProduit, produit.PrixAvantTaxes);
+                        }
                     }
                 }
             }
@@ -90,8 +93,8 @@ namespace StockLibrary
                     {
                         //Affichage de tout les données reliées à un produits
                         Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7}", 
-                            produit.CodeProduit, produit.NomProduit, produit.Quantite,
-                            produit.PrixAvantTaxes, produit.PrixApresTaxes, produit.TPS, produit.TVQ);
+                        produit.CodeProduit, produit.NomProduit, produit.Quantite,
+                        produit.PrixAvantTaxes, produit.PrixApresTaxes, produit.TPS, produit.TVQ);
                     }
                 }
             }
@@ -122,12 +125,66 @@ namespace StockLibrary
                 WriteIndented = true // Pour une sortie JSON formatée de manière lisible
             };
 
-            using (FileStream stream = new FileStream(STOCK_PATH, FileMode.Create))
-            {
-                JsonSerializer.SerializeAsync(stream, mesProduits, options).Wait();
+            /*Si la liste de produits n'est pas vide, les produits voient leurs prix avant taxes être calculer dans le cas où ils sont 
+            //identifiable par leur code de produit*/
+            if (mesProduits != null && mesProduits.Count != 0)
+            { 
+                foreach (Produit produit in mesProduits)
+                {
+                    if (produit.CodeProduit != null && produit.CodeProduit != "")
+                    {
+                        //Correction du prix des produits dans la liste
+                        CorrigerPrixAvantTaxes(produit.CodeProduit);
+                    }
+                    
+                }
             }
-        }
 
+            //Affichage des produits pour confirmer que nous allons sérialiser la bonne liste de produits
+            if (mesProduits != null && mesProduits.Count != 0)
+            {
+                foreach (var produit in mesProduits)
+                {
+                    //Affichage de tout les données reliées à un produits
+                    Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7}",
+                    produit.CodeProduit, produit.NomProduit, produit.Quantite,
+                    produit.PrixAvantTaxes, produit.PrixApresTaxes, produit.TPS, produit.TVQ);
+                }
+            }
+
+
+            //Si l'utilisateur appuie sur Y, la sérialisation commence, sinon elle ne s'effectue pas
+            Console.WriteLine("Les produits suivant sont-il bien ceux à sérialiser? \n Y pour confirmer \t n'importe quelle autre touche pour annuler");
+            ConsoleKey choix = Console.ReadKey().Key;
+
+            if (choix == ConsoleKey.Y)
+            {
+                //Désignation du fichier sur lequel écrire ou qui est à créer
+                using (FileStream stream = new FileStream(STOCK_PATH, FileMode.Create))
+                {
+                    try
+                    {
+                        //Attends que la sérialisation soit compléter avant de poursuivre
+                        JsonSerializer.SerializeAsync(stream, mesProduits, options).Wait();
+                    }
+                    //Gestion des Exceptions
+                    catch (UnauthorizedAccessException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Erreur Innatendue : " + e);
+                    }
+
+                }
+            }
+
+        }
 
     }
 }
