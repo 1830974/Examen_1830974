933aac995c79fd5a6d364d933c12a60f762b1bef
Brandon Tremblay <1830974@etu.cchic.ca>
2023-12-18 13:25:44 -0500

    Push initiale : fichiers de base

---
+++ Examen_Final/StockLibrary/Produit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockLibrary
+{
+    public class Produit
+    {
+        public string? CodeProduit { get; set; }
+        public string? NomProduit { get; set; }
+        public int Quantite { get; set; }
+        public double PrixAvantTaxes { get; set; }
+        public double PrixApresTaxes { get; set; }
+        public double TPS { get; set; }
+        public double TVQ { get; set; }
+
+
+
+        public Produit() { }
+
+        public Produit(string _codeProduit, string _nomProduit, int _quantite, double _prixAvantTaxes, double _prixApresTaxes, double _TPS, double _TVQ)
+        {
+            CodeProduit = _codeProduit;
+            NomProduit = _nomProduit;
+            Quantite = _quantite;
+            PrixAvantTaxes = _prixAvantTaxes;
+            PrixApresTaxes = _prixApresTaxes;
+            TPS = _TPS;
+            TVQ = _TVQ;
+        }
+    }
+}
---
+++ Examen_Final/StockLibrary/StockService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace StockLibrary
+{
+    public class StockService
+    {
+        //Chemin d'accès vers le dossier "Mes Documents" de l'utilisateur actuel
+        private readonly string STOCK_PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\stock-library-data\\liste-produits.json";
+
+        public List<Produit> CorrigerPrixAvantTaxes(string CodeProduit)
+        {
+            FileStream? fs = null;
+            List<Produit>? produits = new List<Produit>();
+
+            // TODO: CorrigerPrixAvantTaxes: compléter,  corriger et commenter
+
+            fs = new FileStream(STOCK_PATH, FileMode.Open);
+
+                produits = JsonSerializer.Deserialize<List<Produit>>(fs);
+
+                if (produits == null || produits.Count == 0)
+                {
+                    Console.WriteLine("Aucun produit trouvé dans le fichier JSON.");
+                }
+                else
+                {
+                    foreach (var produit in produits)
+                    {
+                    Console.WriteLine("{0} {1} {2}", produit.CodeProduit, produit.NomProduit, produit.PrixAvantTaxes);
+                    }
+                }
+
+            return produits;
+        }
+
+        public void ListerLesProduits()
+        {
+            FileStream? fs = null;
+
+            // TODO: ListerLesProduits: compléter,  corriger et commenter
+
+            fs = new FileStream(STOCK_PATH, FileMode.Open);
+               
+                List<Produit> produits = JsonSerializer.Deserialize<List<Produit>>(fs);
+
+                if (produits == null || produits.Count == 0)
+                {
+                    Console.WriteLine("Aucun produit trouvé dans le fichier JSON.");
+                }
+                else
+                {
+                    foreach (var produit in produits)
+                    {
+                        Console.WriteLine("{0} {1} {2}", produit.CodeProduit, produit.NomProduit, produit.PrixAvantTaxes);
+                    }
+                }
+        }
+
+        public void MettreaJourMesProduits(List<Produit> mesProduits)
+        {
+            // TODO: MettreaJourMesProduits: compléter,  corriger et commenter
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true // Pour une sortie JSON formatée de manière lisible
+            };
+
+            using (FileStream stream = new FileStream(STOCK_PATH, FileMode.Create))
+            {
+                JsonSerializer.SerializeAsync(stream, mesProduits, options).Wait();
+            }
+        }
+
+
+    }
+}
